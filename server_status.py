6a582bc318703cd3092b8462bcd857638bc77eb1
hamidreza Farzin <60010616+H-VICTOOR@users.noreply.github.com>
2022-03-12 22:32:06 +0330

    Update server_status.py

--- server_status.py
+++ server_status.py
@@ -6,9 +6,22 @@
 
     Read the descriptions and comments to change the codes and values.
     License :
-    
-    GNU General Public License v3.0 Li
-    http://www.gnu.org/licenses/
+
+    Server Status
+    Copyright (C) <2022> <hamidreza Farzin>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 """
 import asyncio
